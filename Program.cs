95ffeb894af83f5e5dcae47e8c3927be22d9705f
Katsmin <kyokatsmin1909@hotmail.com>
2021-04-12 14:59:42 +0800

    acgt index definition

--- Program.cs
+++ Program.cs
@@ -9,6 +9,19 @@ namespace ChromosomeExtraction
 {
     class Program
     {
+        static readonly double[,] transition ={
+            /* a      c      g      t   */
+            {0.359, 0.143, 0.167, 0.331},
+            {0.384, 0.156, 0.023, 0.437},
+            {0.305, 0.199, 0.150, 0.345 },
+            {0.284, 0.182, 0.177, 0.357 }
+            };
+        const int a = 0;
+        const int c = 1;
+        const int g = 2;
+        const int t = 3;
+
+
         static void Main(string[] args)
         {
             run();
@@ -28,14 +41,7 @@ namespace ChromosomeExtraction
             //    {0.7, 0.3},
             //    {0.4, 0.6}
             //}; 
-            double[,] transition =
-            {
-            /*     a      c      g      t   */
-                {0.359, 0.143, 0.167, 0.331},
-                {0.384, 0.156, 0.023, 0.437},
-                {0.305, 0.199, 0.150, 0.345 },
-                {0.284, 0.182, 0.177, 0.357 }
-            };
+
 
 
             double[,] emission =
@@ -46,7 +52,7 @@ namespace ChromosomeExtraction
                 { 0.2, 0.5, 0.3 }
             };
 
-            double[] initial = { 0.6, 0.4 };
+            double[] initial = { 0.2, 0.25, 0.45, 0.1 };
 
             HiddenMarkovModel hmm = new HiddenMarkovModel(transition, emission, initial);
             int[] sequence = new int[] { 0, 1, 2 };
